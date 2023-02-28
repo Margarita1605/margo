80220a51d948526aea8c03464487268c515c8a1c
Margarita1605 <125960381+Margarita1605@users.noreply.github.com>
2023-02-28 12:26:27 +0400

    Update 1.2_1.py

--- 1.2_1.py
+++ 1.2_1.py
@@ -5,3 +5,23 @@ d=a.shape
 
 print(d)
 
+import numpy as np 
+a = np.array([1.2, 2.4, 3.6, 3.5, 6.7, 1.5, 8.4, 3.1, 9.2]) 
+
+d=a.shape
+
+print(d)
+
+import numpy as np 
+a = np.array([1.2, 2.4, 3.6, 3.5, 6.7, 1.5, 8.4, 3.1, 9.2]) 
+
+d=a.shape
+
+print(d)
+
+import numpy as np 
+a = np.array([1.2, 2.4, 3.6, 3.5, 6.7, 1.5, 8.4, 3.1, 9.2]) 
+
+d=a.shape
+
+print(d)
